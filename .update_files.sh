0916dd00c0231148b5fad7239e7428aee2d80158
Teymur Babayev <me@tymbb.com>
2025-02-28 01:22:48 +0400

    Added fast eftched and modified aliases

--- .update_files.sh
+++ .update_files.sh
@@ -4,8 +4,9 @@ DIR=$(cd `dirname $0` && pwd)
 echo $DIR
 
 cp ~/.bashrc ${DIR}/.bashrc
-cp ~/.profile ${DIR}/.profile
 cp ~/.bash_aliases ${DIR}/.bash_aliases
 cp ~/.tmux.conf ${DIR}/.tmux.conf
 cp ~/.config/neofetch/config.conf ${DIR}/.config/neofetch/config.conf
 cp ~/.config/nvim/init.vim ${DIR}/.config/nvim/init.vim
+
+cp ~/.config/fastfetch/config.jsonc ${DIR}/.config/fastfetch/config.jsonc
--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 - tmux
 - neovim
-- neofetch
-- bash
-- kazam
-- qBittorent
+- neofetch (package is no longer maintaned so switched to fastfetch)
+- fastfetch
+- bashrc, bash_aliases
 
 ### This files are highly personal you will need to edit them out first othwerwise it won't work. Especially .bash_aliases and .bashrc file.
